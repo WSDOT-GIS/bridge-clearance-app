3e49aa5fe8ad36800d3115c65adae03b11dc6e1b
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2014-04-08 17:58:00 +0000

    Added basic map functionality.

---
+++ Script/index.js
@@ -0,0 +1,10 @@
+﻿require(["esri/map"], function (Map) {
+	var map;
+
+	map = new Map("map", {
+		basemap: "streets",
+		center: [-120.80566406246835, 47.41322033015946],
+		zoom: 7,
+		showAttribution: true
+	});
+});
