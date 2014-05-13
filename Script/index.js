9853861a2c45d4562783bf98016f4d2c4c940b81
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2014-05-13 21:01:44 +0000

    Merged revision(s) 44088-44105 from GIS/BridgeDemo/branches/bootstrap: Modified to use bootstrap layout.
    Filtered out fields in popups.

--- Script/index.js
+++ Script/index.js
@@ -13,14 +13,44 @@ require([
 	"esri/Color",
 	"esri/symbols/CartographicLineSymbol",
 	"esri/geometry/webMercatorUtils",
-	"esri/dijit/HomeButton",
 	"esri/renderers/UniqueValueRenderer",
 	"esri/symbols/SimpleMarkerSymbol"
 ], function (Map, esriConfig, domUtils, FeatureLayer, ArcGISTiledMapServiceLayer, Query, InfoTemplate, BasemapGallery,
-	Basemap, BasemapLayer, Color, CartographicLineSymbol, webMercatorUtils, HomeButton, UniqueValueRenderer, SimpleMarkerSymbol) {
+	Basemap, BasemapLayer, Color, CartographicLineSymbol, webMercatorUtils, UniqueValueRenderer, SimpleMarkerSymbol) {
 	var map, bridgeOnLayer, bridgeUnderLayer, onProgress, underProgress, vehicleHeight;
 
 	var fieldsWithWeirdFormatNumbers = /^(?:(?:horiz_clrnc_route)|(?:horiz_clrnc_rvrs)|(?:vert_clrnc_route_max)|(?:vert_clrnc_route_min)|(?:vert_clrnc_rvrs_max)|(?:vert_clrnc_rvrs_min)|(?:min_vert_(?:(?:deck)|(?:under))))$/i;
+
+	$('#tabs a').click(function (e) {
+		e.preventDefault();
+		$(this).tab('show');
+	});
+
+	$("[data-toggle=offcanvas]").click(function () {
+		$(".row-offcanvas").toggleClass('active');
+	});
+
+	/** Set the height of the map div.
+	*/
+	function setMapDivHeight() {
+		var topNavBar, mapDiv, desiredHeight;
+
+		topNavBar = document.getElementById("topNavBar");
+		mapDiv = document.getElementById("map");
+
+		desiredHeight = window.innerHeight - topNavBar.clientHeight - 40;
+
+		mapDiv.style.height = [desiredHeight, "px"].join("");
+
+		//if (map) {
+		// map.resize();
+		//}
+	}
+
+	setMapDivHeight();
+
+	window.addEventListener("resize", setMapDivHeight, true);
+	window.addEventListener("deviceorientation", setMapDivHeight, true);
 
 	/**
 	 * Determines if a vehicle can pass under a structure in ANY lane.
@@ -87,112 +117,10 @@ require([
 
 	// Create the map, explicitly setting the LOD values. (This prevents the first layer added determining the LODs.)
 	map = new Map("map", {
-		showAttribution: true,
-		spatialReference: {
-			wkid: 3857,
-		},
-		lods: [
-			{
-				"level": 0,
-				"resolution": 156543.03392800014,
-				"scale": 5.91657527591555E8
-			},
-			{
-				"level": 1,
-				"resolution": 78271.51696399994,
-				"scale": 2.95828763795777E8
-			},
-			{
-				"level": 2,
-				"resolution": 39135.75848200009,
-				"scale": 1.47914381897889E8
-			},
-			{
-				"level": 3,
-				"resolution": 19567.87924099992,
-				"scale": 7.3957190948944E7
-			},
-			{
-				"level": 4,
-				"resolution": 9783.93962049996,
-				"scale": 3.6978595474472E7
-			},
-			{
-				"level": 5,
-				"resolution": 4891.96981024998,
-				"scale": 1.8489297737236E7
-			},
-			{
-				"level": 6,
-				"resolution": 2445.98490512499,
-				"scale": 9244648.868618
-			},
-			{
-				"level": 7,
-				"resolution": 1222.992452562495,
-				"scale": 4622324.434309
-			},
-			{
-				"level": 8,
-				"resolution": 611.4962262813797,
-				"scale": 2311162.217155
-			},
-			{
-				"level": 9,
-				"resolution": 305.74811314055756,
-				"scale": 1155581.108577
-			},
-			{
-				"level": 10,
-				"resolution": 152.87405657041106,
-				"scale": 577790.554289
-			},
-			{
-				"level": 11,
-				"resolution": 76.43702828507324,
-				"scale": 288895.277144
-			},
-			{
-				"level": 12,
-				"resolution": 38.21851414253662,
-				"scale": 144447.638572
-			},
-			{
-				"level": 13,
-				"resolution": 19.10925707126831,
-				"scale": 72223.819286
-			},
-			{
-				"level": 14,
-				"resolution": 9.554628535634155,
-				"scale": 36111.909643
-			},
-			{
-				"level": 15,
-				"resolution": 4.77731426794937,
-				"scale": 18055.954822
-			},
-			{
-				"level": 16,
-				"resolution": 2.388657133974685,
-				"scale": 9027.977411
-			},
-			{
-				"level": 17,
-				"resolution": 1.1943285668550503,
-				"scale": 4513.988705
-			},
-			{
-				"level": 18,
-				"resolution": 0.5971642835598172,
-				"scale": 2256.994353
-			},
-			{
-				"level": 19,
-				"resolution": 0.29858214164761665,
-				"scale": 1128.497176
-			}
-		]
+		basemap: "streets",
+		center: [-120.80566406246835, 47.41322033015946],
+		zoom: 7,
+		showAttribution: true
 	});
 
 	// Add the WSDOT basemap layer to the map.
@@ -344,7 +272,7 @@ require([
 	 */
 	function toHtmlContent(graphic) {
 		var ignoredFields;
-		ignoredFields = /^(?:(?:\w+_gid)|(?:OBJECTID_?\d*)|(Field\d+)|(Shape_Length))$/i;
+		ignoredFields = /^(?:(?:((OBJECTID_?)|(Field))\d*)|(?:Shape_Length)|(?:\w+(?:(?:code)|(?:class)|(?:Error)|(?:_gid)|(?:indicator)))|(?:(?:(?:lrs)|(?:list))\w+)|(?:(?:min_)?(?:(?:vert)|(?:horiz))\w+)|(?:(?:(?:lateral)|(?:fed)|(?:sort))\w+)|(?:eventID)|(?:agency_id))$/i;
 
 		var fragment = document.createDocumentFragment();
 
@@ -369,7 +297,7 @@ require([
 		var dl = toDL(dlObj);
 
 		fragment.appendChild(dl);
-		var linksHeader = document.createElement("h2");
+		var linksHeader = document.createElement("h3");
 		linksHeader.textContent = "Links";
 		fragment.appendChild(linksHeader);
 
@@ -520,35 +448,31 @@ require([
 		map.addLayer(bridgeOnLayer);
 		map.addLayer(bridgeUnderLayer);
 
-	});
+		// Create the basemap gallery, adding the WSDOT map in addition to the default Esri basemaps.
+		var basemapGallery = new BasemapGallery({
+			map: map,
+			basemaps: [
+				new Basemap({
+					id: "wsdot",
+					title: "WSDOT",
+					thumbnailUrl: "Images/WsdotBasemapThumbnail.jpg",
+					layers: [
+						new BasemapLayer({
+							url: wsdotBasemapUrl
+						})
+					]
+				})
+			],
+			basemapIds: map.layerIds
+		}, "basemapGallery");
+		basemapGallery.startup();
 
-	var homeButton = new HomeButton({ map: map }, document.getElementById("homeButton"));
-	homeButton.startup();
+		// Select the WSDOT basemap when the gallery dijit has loaded.
+		// Hide the basemap gallery. (User will show it by clicking a button.)
+		basemapGallery.on("load", function () {
+			basemapGallery.select("wsdot");
+		});
 
-	// Create the basemap gallery, adding the WSDOT map in addition to the default Esri basemaps.
-	var basemapGallery = new BasemapGallery({
-		map: map,
-		basemaps: [
-			new Basemap({
-				id: "wsdot",
-				title: "WSDOT",
-				thumbnailUrl: "Images/WsdotBasemapThumbnail.jpg",
-				layers: [
-					new BasemapLayer({
-						url: wsdotBasemapUrl
-					})
-				]
-			})
-		],
-		basemapIds: ["wsdot"]
-	}, "basemapGallery");
-	basemapGallery.startup();
-
-	// Select the WSDOT basemap when the gallery dijit has loaded.
-	// Hide the basemap gallery. (User will show it by clicking a button.)
-	basemapGallery.on("load", function () {
-		basemapGallery.select("wsdot");
-		domUtils.hide(basemapGallery.domNode);
 	});
 
 	// Set up the progress bar to show when the map is loading.
@@ -559,11 +483,6 @@ require([
 	map.on("update-start", function () {
 		domUtils.show(document.getElementById("mapProgress"));
 	});
-
-	// Set up the button that will show and hide the basemap gallery.
-	document.getElementById("basemapsToggleButton").onclick = function () {
-		domUtils.toggle(basemapGallery.domNode);
-	};
 
 	/**
 	 * Creates a layer definition string
